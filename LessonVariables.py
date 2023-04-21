2843daff499bba327a0190aa8338cc45bc36e220
grnoharutyunyan <77321096+grnoharutyunyan@users.noreply.github.com>
2023-04-21 22:41:50 +0400

    Add files via upload

---
+++ LessonVariables.py
@@ -0,0 +1,20 @@
+a = 'Hello'
+b = 25
+print(a)
+print(b)
+
+f_name = 'Denis'
+l_name = 'Pupkin'
+age = 25
+full_name = f_name + " " + l_name
+print(full_name)
+#print(f_name + " " + l_name)
+print(full_name + " " + str(age))
+
+f_name = "denis"
+F_name = "DENIS"
+print(f_name + "------" +  F_name)
+num1= 555
+num2 = 4444
+num3 = num1 + num2
+print(num3)
